1a4d0ee0552818b0d536b1417d8a97c33f878005
Samuel Davi <samuel.dvmonteiro@gmail.com>
2024-10-14 12:55:55 -0300

    ajustando configuração do docker (Dockerfiles e docker-compose)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (Request $request) {
+    
     return response()->json(User::find(1));
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,22 @@ import { useEffect, useState } from "react"
 function App() {
 
     const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
 
     useEffect(() => {
 
-        fetch('http://localhost/api', {
-            method: 'GET', // or 'POST', 'PUT', etc.
-        }).then(resp => resp.json()).then(json => {
+        fetch('http://localhost/api').then(resp => resp.json()).then(json => {
 
            setName(json.name)
+           setEmail(json.email)
         });
     }, []);
 
     return (
         <>
             <h2>Nome: {name}</h2>
+            <h2>Email: {email}</h2>
+
         </>
     )
 }
